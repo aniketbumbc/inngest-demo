263c536a8c94366ad586bca704484a144ce32e41
aniketbumbc <aniket.umbc@gmail.com>
2026-09-12 16:47:15 +0530

    initial function created for inngest

---
+++ basic-1/src/index.ts
@@ -0,0 +1,63 @@
+import { Inngest } from 'inngest';
+
+export const inngest = new Inngest({ id: 'test-app' });
+
+const helloWorld = inngest.createFunction(
+  { id: 'hello-world', triggers: { event: 'test/hello-world' } },
+  async ({ event, step }) => {
+    console.log('Hello, world!');
+    console.log(event.name);
+    console.log(event.data);
+    return {
+      message: `${event.name} ${event.data} || Hello World`,
+      recievedAt: new Date().toISOString(),
+    };
+  },
+);
+
+console.log('Function created', helloWorld.id());
+
+const multistepFunction = inngest.createFunction(
+  { id: 'multistep-function-demo', triggers: { event: 'test/multistep-function-demo' } },
+  async ({ event, step }) => {
+    const firstTask = await step.run('step-1', async () => {
+      // step 1 create 1 task hold it
+      console.log(' Executing Step 1');
+      return {
+        message: 'Step 1 executed',
+        recievedAt: new Date().toISOString(),
+      };
+    });
+
+    console.log('First task result ', firstTask);
+
+    const secondTask = await step.sleep('step-2-wait', 5000, async () => {
+      console.log(' Executing Step 2');
+      return {
+        message: `Step 2 executed ${firstTask.message}`,
+        recievedAt: new Date().toISOString(),
+      };
+    });
+    console.log('Second task result ', secondTask);
+
+    // step 3
+    const thirdTask = await step.run('step-3', async () => {
+      console.log(' Executing Step 3');
+      return {
+        message: `Step 3 executed ${secondTask.message}`,
+        previousTask: {
+          message: [secondTask.message, firstTask.message],
+          recievedAt: [secondTask.recievedAt, firstTask.recievedAt],
+        },
+        recievedAt: new Date().toISOString(),
+      };
+    });
+    console.log('Third task result ', thirdTask);
+
+    return {
+      message: `Multistep function executed from async step ${firstTask.message} ${secondTask.message} ${thirdTask.message}`,
+      recievedAt: new Date().toISOString(),
+    };
+  },
+);
+console.log('Multistep Function created', multistepFunction.id());
